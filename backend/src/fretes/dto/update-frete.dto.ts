e9de9c44dce5b3c2d62484a16a26084f2ad1a281
brenddonanjos <brenddon.dev@gmail.com>
2024-11-27 16:38:25 -0300

    delete nest

--- backend/src/fretes/dto/update-frete.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFreteDto } from './create-frete.dto';
-
-export class UpdateFreteDto extends PartialType(CreateFreteDto) {}
--- backend/src/fretes/fretes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FretesService } from './fretes.service';
-import { FretesController } from './fretes.controller';
-
-@Module({
-  controllers: [FretesController],
-  providers: [FretesService],
-})
-export class FretesModule {}
